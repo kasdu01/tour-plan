24e0c10a161be555980277387d373d99e68bab6d
kasdu01 <kasdu01@yandex.ru>
2021-09-23 03:51:50 +0400

    create: added reviews section

--- js/main.js
+++ js/main.js
@@ -13,3 +13,19 @@ const hotelSwiper = new Swiper('.hotel-slider', {
     pageUpDown: true,
   },
 });
+
+const reviewsSlider = new Swiper('.reviews-slider', {
+  // Optional parameters
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.reviews-slider__button--next',
+    prevEl: '.reviews-slider__button--prev',
+  },
+   keyboard: {
+    enabled: true,
+    onlyInViewport: false,
+    pageUpDown: true,
+  },
+});
