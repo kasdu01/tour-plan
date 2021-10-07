b9ac533d168130e3528bf5074582d90fb19bf4dc
kasdu01 <kasdu01@yandex.ru>
2021-10-07 17:28:11 +0300

    fix: validate phone

--- js/main.js
+++ js/main.js
@@ -58,15 +58,48 @@ $(document).ready(function () {
     var modalDialog = $(".modal__dialog");
     modalOverlay.addClass("modal__overlay--visible");
     modalDialog.addClass("modal__dialog--visible");
+    $('body').css('overflow', 'hidden');
   };
   
   // функция закрытия окна
   function closeModal(event) {
+    $('body').css('overflow', 'auto');
     event.preventDefault();
     var modalOverlay = $(".modal__overlay");
     var modalDialog = $(".modal__dialog");
     modalOverlay.removeClass("modal__overlay--visible");
     modalDialog.removeClass("modal__dialog--visible");
   };
+
+  // обработка валидации форм
+  $('.form').each(function() {
+  $(this).validate({
+    errorClass: "invalid",
+    messages: {
+      name: {
+        required: "Please specify your name",
+        minlength: "Minimal lenght of name 2 symbol"
+      },
+      phone: {
+        required: "Please input your phone"
+      },
+      email: {
+        required: "We need your email address to contact you",
+        email: "Your email address must be in the format of name@domain.com"
+      }
+    }
+  });    
+  });
+
+  $(document).ready(function(){
+    $(".input-phone").click(function(){
+    $(this).setCursorPosition(3);
+    }).mask("+7(999) 999-9999");
+    });
+
+  $('.input-phone').on('click', function() {
+    $('.input-phone').attr("placeholder", "+7 (999) 999-9999");
+    });
+  
 
 });
