cad5cf2099fbd6b8488c407a51abc73d2f98e9b4
kasdu01 <kasdu01@yandex.ru>
2021-10-09 02:12:22 +0300

    fix: pixel perfect final

--- js/main.js
+++ js/main.js
@@ -122,4 +122,16 @@ $(document).ready(function () {
     }
   });
 
+  const map = $('.map');
+    if (!map.attr('data-load')) {
+    map.mouseenter(() => {
+    map.attr('data-load', true);
+    map.replaceWith(`<iframe class="map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5676522.168545385!2d-82.22684935246507!3d24.845122340293962!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xdfd2f9e85f1f29b7!2sHilton%20Miami%20Downtown!5e0!3m2!1sru!2sru!4v1607509293969!5m2!1sru!2sru" aria-hidden="false" tabindex="0"></iframe>`);
+     });
+       map.click(() => {
+    map.attr('data-load', true);
+    map.replaceWith(`<iframe class="map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5676522.168545385!2d-82.22684935246507!3d24.845122340293962!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xdfd2f9e85f1f29b7!2sHilton%20Miami%20Downtown!5e0!3m2!1sru!2sru!4v1607509293969!5m2!1sru!2sru" aria-hidden="false" tabindex="0"></iframe>`);
+    });
+  }
+
 });
